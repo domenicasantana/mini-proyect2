68230135832382e00d667068f2e6fdf83c038751
domenicasantana <dasc1996@hotmail.com>
2023-04-20 15:18:52 +0000

    add changes in route decorator

--- run.py
+++ run.py
@@ -30,8 +30,10 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print("hello")
     return render_template("contact.html", page_title="Contact")
 
 
